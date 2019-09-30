fc30637ccd8b48172522d5ea177c8ad06697f88a
1510460325@qq.com <1510460325@qq.com>
2019-09-30 22:21:40 +0800

    [modify] 改为直接判题

--- runner/test.c
+++ runner/test.c
@@ -8,9 +8,29 @@
 #include <sys/ptrace.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <sys/wait.h>
+#include <sys/wait.h> 
 
-void setProcessLimit(long timelimit, long memory_limit) {
+
+#define AC 0
+#define PE 1
+#define TLE 2
+#define MLE 3
+#define WA 4
+#define RE 5
+#define OLE 6
+#define CE 7
+#define SE 8
+
+struct result {
+    int status;
+    int timeUsed;
+    int memoryUsed;
+};
+
+/*
+set the process limit(cpu and memory)
+*/
+void setProcessLimit(int timelimit, int memory_limit) {
     struct rlimit rl;
     /* set the time_limit (second)*/
     rl.rlim_cur = timelimit / 1000;
@@ -23,80 +43,105 @@ void setProcessLimit(long timelimit, long memory_limit) {
     setrlimit(RLIMIT_DATA, &rl);
 }
 
-int run(char *args[],long timelimit, long memory_limit, char *in, char *out){
+/*
+ run the user process
+*/
+void runCmd(char *args[],long timeLimit, long memoryLimit, char *in, char *out) {
+    int newstdin = open(in,O_RDWR|O_CREAT,0644);
+    int newstdout = open(out,O_RDWR|O_CREAT,0644);
+    setProcessLimit(timeLimit, memoryLimit);
+    if (newstdout != -1 && newstdin != -1){
+        dup2(newstdout,fileno(stdout));
+        dup2(newstdin,fileno(stdin));
+        if (execvp(args[0],args) == -1) {
+            printf("====== Failed to start the process! =====\n");
+        }
+        close(newstdin);
+        close(newstdout);
+    } else {
+        printf("====== Failed to open file! =====\n");
+    }
+}
+
+/*
+get data of the user process
+*/
+void getResult(struct rusage *ru, int data[2]) {
+    data[0] = ru->ru_utime.tv_sec * 1000
+            + ru->ru_utime.tv_usec / 1000
+            + ru->ru_stime.tv_sec * 1000
+            + ru->ru_stime.tv_usec / 1000;
+    data[1] = ru->ru_maxrss;
+}
+
+/*
+monitor the user process
+*/
+void monitor(pid_t pid, int timeLimit, int memoryLimit, struct result *rest) {
+    int status;
+    struct rusage ru;
+    if (wait4(pid, &status, 0, &ru) == -1)
+        printf("wait4 failure");
+    rest->timeUsed = ru.ru_utime.tv_sec * 1000
+            + ru.ru_utime.tv_usec / 1000
+            + ru.ru_stime.tv_sec * 1000
+            + ru.ru_stime.tv_usec / 1000;
+    rest->memoryUsed = ru.ru_maxrss;
+    if (WIFSIGNALED(status)) {
+        switch (WTERMSIG(status)) {
+            case SIGSEGV:
+                if (rest->memoryUsed > memoryLimit)
+                    rest->status = MLE;
+                else
+                    rest->status = RE;
+                break;
+            case SIGALRM:
+            case SIGXCPU:
+                rest->status = TLE;
+                break;
+            default:
+                rest->status = RE;
+                break;
+        }
+    } else {
+        if (rest->timeUsed > timeLimit)
+            rest->status = TLE;
+        else if (rest->memoryUsed > memoryLimit)
+            rest->status = MLE;
+        else
+            rest->status = AC;
+    }
+}
+
+
+int run(char *args[],int timeLimit, int memoryLimit, char *in, char *out){
     pid_t pid = vfork();
     if(pid<0)
         printf("error in fork!\n");
     else if(pid == 0) {
-        int newstdin = open(in,O_RDWR|O_CREAT,0644);
-        int newstdout=open(out,O_RDWR|O_CREAT,0644);
-        if (newstdout != -1 && newstdin != -1){
-            if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) != -1) {
-                printf("====== ok =====\n");
-            }
-            dup2(newstdout,fileno(stdout));
-            dup2(newstdin,fileno(stdin));
-            setProcessLimit(timelimit, memory_limit);
-            if (execvp(args[0],args) == -1) {
-                printf("execvp is error!\n");
-            }
-            close(newstdin);
-            close(newstdout);
-        } else {
-            printf("====== error =====\n");
-        }
+        runCmd(args, timeLimit, memoryLimit, in, out);
     } else {
-        struct rusage ru;
-        int status, time_used = 0, memory_used = 0;
-        printf("the child pid is %d \n", pid);
-        while (1) {
-            if (wait4(pid, &status, WUNTRACED, &ru) == -1)
-                printf("wait4 [WSTOPPED] failure");
-            if (WIFEXITED(status)) {
-                time_used = ru.ru_utime.tv_sec * 1000
-                        + ru.ru_utime.tv_usec / 1000
-                        + ru.ru_stime.tv_sec * 1000
-                        + ru.ru_stime.tv_usec / 1000;
-                memory_used = ru.ru_maxrss;
-                printf("child process is right!\n");
-                printf("timeused: %d ms | memoryused : %d b\n",time_used,memory_used);
-                return 1;
-            }
-            else if (WSTOPSIG(status) != SIGTRAP) {
-                ptrace(PTRACE_KILL, pid, NULL, NULL);
-                waitpid(pid, NULL, 0);
-                time_used = ru.ru_utime.tv_sec * 1000
-                        + ru.ru_utime.tv_usec / 1000
-                        + ru.ru_stime.tv_sec * 1000
-                        + ru.ru_stime.tv_usec / 1000;
-                memory_used = ru.ru_maxrss;
-                switch (WSTOPSIG(status)) {
-                    case SIGSEGV:
-                        if (memory_used > memory_limit)
-                            printf("ME\n");
-                        else
-                            printf("RE\n");
-                        break;
-                    case SIGALRM:
-                    case SIGXCPU:
-                        printf("TLE\n");
-                        break;
-                    default:
-                        printf("RE\n");
-                        break;
-                }
-                printf("child process is wrong!\n");
-                printf("timeused: %d ms | memoryused : %d b\n",time_used,memory_used);
-                return 0;
-            }
-            ptrace(PTRACE_CONT, pid, NULL, NULL);
-        }
-        return -1;
+        struct result rest;
+        monitor(pid, timeLimit, memoryLimit, &rest);
+        printf("{\"status\":\"%d\",\"timeUsed:\":\"%d\",\"memoryUsed:\":\"%d\"}", rest.status, rest.timeUsed, rest.memoryUsed);
     }
 }
-int main()
+
+void split( char **arr, char *str, const char *del){
+    char *s =NULL;
+    s = strtok(str,del);
+    while(s != NULL)
+    {
+        *arr++ = s;
+        s = strtok(NULL,del);
+    }
+    *arr++ = NULL;
+}
+
+int main(int argc,char *argv[])
 {
-    char *args[] = {"/home/hadoop/tmp/demo",NULL};
-    run(args,1000,1000,"0.in","0.out");
+    char *cmd[20];
+    split(cmd, argv[1], "@");
+    run(cmd, atoi(argv[2]), atoi(argv[3]), argv[4], argv[5]);
     return 0;
 }
